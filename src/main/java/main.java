c6e8b17f03b8ff44e032daec90e2a00adace1a58
Jemma <jay.poff@outlook.com>
2019-04-26 13:47:24 -0500

    Update main.java
    changed class to use loadConfig method instead

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,6 @@
 public class main {
     public static void main(String[] args) {
         miiDataBase.initMii();
-        xmlBuilder.makeWWP(
-                "",
-                "",
-                "",
-                "",
-                "");
+        fRead.loadConfig();
     }
 }
