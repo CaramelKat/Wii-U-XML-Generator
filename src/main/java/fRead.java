f2cdfb1ed368116e24f98a32d31febf5030d6cb5
CaramelKat <32065563+CaramelKat@users.noreply.github.com>
2019-04-26 13:35:13 -0500

    Fixed a couple of bugs and added the ability to load the required information from a config file.

--- src/main/java/fRead.java
+++ src/main/java/fRead.java
@@ -4,16 +4,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class fRead {
-    public static String readFile(String file){
+    public static String readFile(String txt) {
         String result = "";
         try {
             //creating FileReader object.
-            FileReader fr = new FileReader(file);
+            FileReader fr =
+                    new FileReader(txt);
+
             int i;
             //read file.
             while((i=fr.read())!=-1){
                 result += (char)i;
-            }return result;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,6 +42,63 @@ public class fRead {
         //CharMatcher legal = CharMatcher.javaLetterOrDigit();
         //String out = legal.removeFrom(s);
         return new String (bytes);
+    }
+    public static void loadConfig() {
+        int pipeIndex = -1;
+        String file = fRead.readFile("config.txt");
+        String userName = "";
+        String password = "";
+        String clientID = "";
+        String clientSecret = "";
+        String appID = "";
+        if (file != null) {
+            pipeIndex = file.indexOf("|");
+        }
+        while (pipeIndex != -1) {
+            userName = file.substring(0, file.indexOf("|"));
+            file = file.substring(pipeIndex + 1, file.length());
+
+            pipeIndex = file.indexOf("|");
+            if (pipeIndex == -1) break;
+            password = file.substring(0, pipeIndex);
+            file = file.substring(pipeIndex + 1, file.length());
+
+            pipeIndex = file.indexOf("|");
+            if (pipeIndex == -1) break;
+            clientID = file.substring(0, pipeIndex);
+            file = file.substring(pipeIndex + 1, file.length());
+
+            pipeIndex = file.indexOf("|");
+            if (pipeIndex == -1) break;
+            clientSecret = file.substring(0, pipeIndex);
+            file = file.substring(pipeIndex + 1, file.length());
+
+            pipeIndex = file.indexOf("|");
+            if (pipeIndex == -1) break;
+            appID = file.substring(0, pipeIndex);
+            file = file.substring(pipeIndex + 1, file.length());
+
+            pipeIndex = file.indexOf("|");
+            if (pipeIndex == -1) break;
+        }
+        if (userName.equals("")) {
+            System.out.println("unable to get username");
+        } else if (password.equals("")) {
+            System.out.println("unable to get password");
+        } else if (clientID.equals("")) {
+            System.out.println("unable to get clientID");
+        } else if (clientSecret.equals("")) {
+            System.out.println("unable to get clientSecret");
+        } else if (appID.equals("")) {
+            System.out.println("unable to get appID");}
+         else {
+            xmlBuilder.makeWWP(
+                    userName,
+                    password,
+                    clientID,
+                    clientSecret,
+                    appID);
+        }
     }
 
 }
